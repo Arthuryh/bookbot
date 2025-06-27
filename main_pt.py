10549b4f13eb7bed391a713f8cbe469829e5e5b9
Arthuryh <ayhayakawa@gmail.com>
2025-06-26 21:42:08 -0300

    main.py portuguese version, optimal logic in last for in main.py

--- main_pt.py
+++ main_pt.py
@@ -8,21 +8,21 @@ def get_book_text(filepath):
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Como usar: python3 main.py <caminho absoluto arquivo>")
         sys.exit(1)
     else:
-        print("============ BOOKBOT ============")
-        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("================= BOOKBOT =================")
+        print(f"Analisando o livro encontrado em {sys.argv[1]}...")
         num_words = count_words(get_book_text(sys.argv[1]))
         num_charac = count_characters(get_book_text(sys.argv[1]))
         sorted_list = sort_list(num_charac)
-        print("----------- Word Count ----------")
-        print(f"Found {num_words} total words")
-        print("--------- Character Count -------")
+        print("----------- Contagem de Palavras ----------")
+        print(f"Encontrado um total de {num_words} palavras")
+        print("---------- Contagem de Caracteres --------")
         for i in sorted_list:
             if i["name"].isalpha() == True:
                 print(f"{i["name"]}: {i["num"]}")
             else:
                 pass
-        print("============= END ===============")
+        print("================== FIM ===================")
 main()
